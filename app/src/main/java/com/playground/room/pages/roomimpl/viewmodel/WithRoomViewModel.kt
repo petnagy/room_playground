0fe7a7992b18bf8ba6da43f8c6122fd6865f87b3
Mihaly Hunyady <mihaly_hunyady@epam.com>
2018-02-26 10:00:55 +0100

    change notifyChange to notifyPropertyChange

--- app/src/main/java/com/playground/room/pages/roomimpl/viewmodel/WithRoomViewModel.kt
+++ app/src/main/java/com/playground/room/pages/roomimpl/viewmodel/WithRoomViewModel.kt
@@ -1,7 +1,9 @@
 package com.playground.room.pages.oldsqlite.viewmodel
 
 import android.databinding.BaseObservable
+import android.databinding.Bindable
 import android.view.View
+import com.playground.room.BR
 import com.playground.room.common.recyclerview.ListItemViewModel
 import com.playground.room.pages.oldsqlite.model.WithRoomModel
 import com.playground.room.pages.roomimpl.viewmodel.RoomCharCardViewModel
@@ -11,20 +13,21 @@ import com.playground.room.pages.roomimpl.viewmodel.RoomCharCardViewModel
  */
 class WithRoomViewModel(private val model: WithRoomModel) : BaseObservable() {
 
+    @Bindable
     var chars: MutableList<ListItemViewModel> = mutableListOf()
 
     fun onShowAllClicked(view: View) {
         chars = model.getAllChars().map { it -> RoomCharCardViewModel(it) }.toMutableList()
-        notifyChange()
+        notifyPropertyChanged(BR.chars)
     }
 
     fun onEpireClicked(view: View) {
         chars = model.getEmpireChars().map { it -> RoomCharCardViewModel(it) }.toMutableList()
-        notifyChange()
+        notifyPropertyChanged(BR.chars)
     }
 
     fun onRebelesClicked(view: View) {
         chars = model.getRebelChars().map { it -> RoomCharCardViewModel(it) }.toMutableList()
-        notifyChange()
+        notifyPropertyChanged(BR.chars)
     }
 }
